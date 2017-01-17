c17c6a1713e68b5edc7740fd6f9f6988c8553cc7
Habib Khan <hkhan@smbhd.com>
2017-01-17 14:10:39 -0600

    made inital changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{userName}}</h1>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>Name:<input value="{{hero.name}}" placeholder="Name"/></label>{{hero.name}}</div>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    userName = 'Habib';
+   
+   hero ={
+     name: 'Batman',
+     id: 1
+   }; 
+  }
